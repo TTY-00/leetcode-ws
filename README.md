b2e3118f0f785350f939b0c89b2beabb20797d62
TTY-00 <ttea.yin@gmail.com>
2019-10-29 23:15:35 +0800

    another timeout solution to threesum

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # leetcode-ws
 
 ## 数组与哈希表
-lc index|problem|tag|difficulty|better solution|cpp|py|
+LC index|problem|tag|difficulty|better solution|cpp|py|
 :-:|:-:|:-:|:-:|:-:|:-:|:-:
 1|Two Sum|array|easy|√|√|
-15|Two Sum|array|medium|||√
+15|Three Sum|array|medium|||√
--- threesum.py
+++ threesum.py
@@ -20,3 +20,59 @@ class Solution(object):
                             result.append(temp)
                             
         return result
+
+    # timeout
+    def twoSum(self, nums):
+        self.two_map = {}
+        self.two_num_map = {}
+        
+        # print(nums)
+        for i, ni in enumerate(nums):
+            for j, nj in enumerate(nums[i + 1:]):
+                sum_two = ni + nj
+                if sum_two not in self.two_map:
+                    self.two_map[sum_two] = []
+                if sum_two not in self.two_num_map:
+                    self.two_num_map[sum_two] = []
+                    
+                if ni <= nj and [ni, nj] not in self.two_num_map[sum_two]:
+                    self.two_map[sum_two].append([i, j + i + 1])
+                    self.two_num_map[sum_two].append([ni, nj])
+                    
+                if ni > nj and [nj, ni] not in self.two_num_map[sum_two]:
+                    self.two_map[sum_two].append([j + i + 1, i])
+                    self.two_num_map[sum_two].append([nj, ni])
+                    
+    # timeout            
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.twoSum(nums)
+
+        result_map = {}
+        result = []
+        
+        
+        for i, n in enumerate(nums):
+            diff = 0 - n
+            if not diff in self.two_map:
+                continue
+                
+            for r in self.two_map[diff]:
+                
+                if i in r:
+                    continue
+
+                if n < nums[r[0]]:
+                    temp = [n, nums[r[0]], nums[r[1]]]
+                elif n > nums[r[1]]:
+                    temp = [nums[r[0]], nums[r[1]], n]
+                else:
+                    temp = [nums[r[0]], n, nums[r[1]]]
+                    
+                result.append(temp)
+                    
+            
+        return set(result)
